ac75d03dd34d74427e8f11c966dc8e50570c33a1
camsr904 <camariboy1995@gmail.com>
2025-04-14 13:30:58 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-# Fistname Lastname
-# 11/15/2024
+# Camari Boyd
+# 4/14/2025
 # A simple Python program to print a message
 
+# Fistname Lastname
+# 4/14/2025
+# I added more commentary
+
 print("Hello, World")
+
+print("Welcome to GitHub")
